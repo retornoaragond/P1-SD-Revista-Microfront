ea3f33e7a777eb9ef4506a857d343eb95ce0d385
mark532 <mmurillo532@gmail.com>
2021-10-16 12:56:22 -0600

    E. Gitignore
    Se agrega el details, ademas del git ignore

--- revistadetails.vue
+++ revistadetails.vue
@@ -4,16 +4,26 @@
         <form>
             <div class="form-group">
                 <label for="nameInput">Name:</label>
-                <input type="text" class="form-control" readonly name="name" v-model='this.ilustrator.name'>
+                <input type="text" class="form-control" readonly name="name" v-model='this.revista.name'>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="nationalityInput">Nationality:</label>
-                    <input type="text" class="form-control" readonly name="nationality" v-model='this.ilustrator.nationality'>
+                    <input type="text" class="form-control" readonly name="nationality" v-model='this.revista.edition'>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="birthdayInput">Birthday:</label>
-                    <input type="text" class="form-control" readonly name="birthday" v-model='this.ilustrator.birthday'>
+                    <input type="text" class="form-control" readonly name="birthday" v-model='this.revista.birthday'>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="nationalityInput">Nationality:</label>
+                    <input type="text" class="form-control" readonly name="nationality" v-model='this.revista.language'>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="birthdayInput">Birthday:</label>
+                    <input type="text" class="form-control" readonly name="birthday" v-model='this.revista.birthday'>
                 </div>
             </div>
         </form>
@@ -24,9 +34,9 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="comic in this.ilustrator.comics">                       
+                <tr v-for="char in this.revista.characters">                       
                     <td>
-                        <li><a :href="'https://p1-sd-revistas-microservice.herokuapp.com/revista/'+comic.comic_id">{{comic.comic_name}}</li>
+                        <li><a :href="'https://p1-sd-revistas-microservice.herokuapp.com/revista/'+char.character_id">{{char.character_name}}</li>
                         <!-- Cambiar por el microfront end -->
                     </td>
                 </tr>
@@ -42,23 +52,26 @@ module.exports = {
     props:['title'],
     data:function () {
         return {
-            ilustrator:[]
+            revista:[]
         }
     },
     created () {
-        this.getIlustrator(app.$route.params.id)
+        this.getRevistas(app.$route.params.id)
     },
     methods: {
-        getIlustrator(id){
-            fetch('https://p1-sd-ilustrador-microservice.herokuapp.com/ilustrador/'+id,{ headers: {'Accept': 'application/json'}})
+        getRevistas(id){
+            fetch('https://p1-sd-revistas-microservice.herokuapp.com/revista/'+id,{ headers: {'Accept': 'application/json'}})
             .then((response) => response.json())
             .then((data) => {
-                this.ilustrator = data;
-                console.log('Print: ' + this.ilustrator.name);
+                this.revista = data;
+                console.log('Print: ' + this.revista.name);
             })
             .catch(function (error) {
                 console.log('Error f1: ' + error);
             })
+        },
+        getilustrator(id){
+
         }
     }
 }
